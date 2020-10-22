6af067dc3b4f4ee88f7d1120759ea1ba7428a9ed
Renato Martins <rochamartinsrenato@gmail.com>
2020-10-22 10:55:18 +0100

    Added a new line of text on each code run

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,18 @@
 import random
 def primary():
-  f = open("quotes.txt")
+  f = open("quotes.txt", "r")
   quotes = f.readlines()
   f.close()
+  
+  f = open("quotes.txt", "a")
+  f.write(f"\nThis is line number {len(quotes) + 1}")
+  f.close()
 
-  last = 17
+  last = len(quotes)
   randomlist = random.sample(range(0, last), 2)
 
-  print(quotes[randomlist[0]], end = "" )
-  print(quotes[randomlist[1]])
+  print(quotes[randomlist[0]], end = "")
+  print(quotes[randomlist[1]], end = "")
 
 if __name__== "__main__":
   primary()
